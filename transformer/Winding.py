4aca07bb381da15d4f68ab6cd717e74f073bbfd0
mpegjohn <john_wilkinson3@sky.com>
2016-12-19 15:32:07 +0000

    trying to do a query

---
+++ transformer/Winding.py
@@ -0,0 +1,18 @@
+import  transformers.models
+
+
+class Winding:
+
+    def __init__(self, voltage, current, current_density):
+        self.voltage = voltage
+        self.current = current
+        self.current_density = current_density
+
+    def calculate_wire_size(self)
+        
+
+
+
+
+
+
--- transformer/views.py
+++ transformer/views.py
@@ -29,6 +29,24 @@ def transformer_data(request):
         form = TransformerForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
+
+            primary_voltage = form.cleaned_data['primary_voltage']
+            s1_voltage = form.cleaned_data['s1_voltage']
+	    s1_current = form.cleaned_data['s1_current']
+
+
+            # Current densityu A/(mm^2)
+            CURRENT_DENSITY = 3
+
+	    windings{'primary':'voltage'} = form.cleaned_data['primary_voltage']
+
+	     
+
+
+
+
+
+
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
