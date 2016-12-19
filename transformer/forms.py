357f5b8e0f5571e7c99d1840c01ba177801fcf64
mpegjohn <john_wilkinson3@sky.com>
2016-12-19 14:40:06 +0000

    Secondary selection working

--- transformer/forms.py
+++ transformer/forms.py
@@ -11,13 +11,13 @@ class TransformerForm(forms.Form):
 	s1_current = forms.FloatField(label='S1 current', required=False)
 	s2_enabled = forms.BooleanField(label='S2 enabled', initial=False, required=False)
 	s2_voltage = forms.FloatField(label='S2 voltage', required=False, disabled=True)
-	s2_current = forms.FloatField(label='S2 current', required=False)
+	s2_current = forms.FloatField(label='S2 current', required=False, disabled=True)
 	s3_enabled = forms.BooleanField(label='S3 enabled', initial=False, required=False)
-	s3_voltage = forms.FloatField(label='S3 voltage', required=False)
-	s3_current = forms.FloatField(label='S3 current', required=False)
+	s3_voltage = forms.FloatField(label='S3 voltage', required=False, disabled=True)
+	s3_current = forms.FloatField(label='S3 current', required=False, disabled=True)
 	s4_enabled = forms.BooleanField(label='S4 enabled', initial=False, required=False)
-	s4_voltage = forms.FloatField(label='S4 voltage', required=False)
-	s4_current = forms.FloatField(label='S4 current', required=False)
+	s4_voltage = forms.FloatField(label='S4 voltage', required=False, disabled=True)
+	s4_current = forms.FloatField(label='S4 current', required=False, disabled=True)
 	
 
 
