1b2dbeb085125325835d626ac3a8fefda980db52
mpegjohn <john_wilkinson3@sky.com>
2016-12-20 15:42:51 +0000

    using trace to pause, got wire selection working

--- transformer/views.py
+++ transformer/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 from .forms import NameForm, TransformerForm
+from reactor.models import Wire
+
 
 def get_name(request):
     # if this is a POST request we need to process the form data
@@ -30,6 +32,9 @@ def transformer_data(request):
         # check whether it's valid:
         if form.is_valid():
 
+
+            import pdb; pdb.set_trace()
+
             primary_voltage = form.cleaned_data['primary_voltage']
             s1_voltage = form.cleaned_data['s1_voltage']
 	    s1_current = form.cleaned_data['s1_current']
@@ -37,15 +42,21 @@ def transformer_data(request):
 
             # Current densityu A/(mm^2)
             CURRENT_DENSITY = 3
+	    windings = {}
+            windings['primary'] = {}
 
-	    windings{'primary':'voltage'} = form.cleaned_data['primary_voltage']
+	    windings['primary']['voltage'] = form.cleaned_data['primary_voltage']
 
-	     
+            windings['secondary'] = {}
+	    windings['secondary'][1] = {}
+            windings['secondary'][1]['voltage'] = form.cleaned_data['s1_voltage']
+            windings['secondary'][1]['current'] = form.cleaned_data['s1_current']
 
+            calc_area = form.cleaned_data['s1_current']/CURRENT_DENSITY
 
-
-
-
+            # get the first wire that has this area or greater.
+            Wire.objects.filter(area__gte = calc_area)[:1]
+           
 
             # process the data in form.cleaned_data as required
             # ...
